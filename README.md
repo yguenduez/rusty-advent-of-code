74362a1edf7e8d83276e0b550ab1820db64c67d4
YasinGuenduez-EH <yasinguenduez1@endress.com>
2022-12-01 11:29:22 +0100

    Add Solution for second puzzle

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ The puzzles are given here: [Advent of Code](https://adventofcode.com/)
 
 #### Day 1
 
+##### Part 1
+
 Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
 
 To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
@@ -49,3 +51,24 @@ In case the Elves get hungry and need extra snacks, they need to know which Elf 
 Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 
 Answer: 67622
+
+##### Part 2
+
+By the time you calculate the answer to the Elves' question,
+they've already realized that the Elf carrying the most Calories of 
+food might eventually run out of snacks.
+
+To avoid this unacceptable situation, the Elves would 
+instead like to know the total Calories carried by the top 
+three Elves carrying the most Calories. That way, even if 
+one of those Elves runs out of snacks, they still have two backups.
+
+In the example above, the top three Elves are the fourth Elf
+(with 24000 Calories), then the third Elf (with 11000 Calories), 
+then the fifth Elf (with 10000 Calories). The sum of the Calories 
+carried by these three elves is 45000.
+
+Find the top three Elves carrying the most Calories. How many 
+Calories are those Elves carrying in total?
+
+Answer: 201491
--- src/day_1/puzzle_1.rs
+++ src/day_1/puzzle_1.rs
@@ -1,9 +1,10 @@
 use crate::common::file_io;
+use std::collections::BinaryHeap;
 
 struct Solution;
 
 impl Solution {
-    fn solve_puzzle(input_file: &str) -> u64 {
+    fn solve_first_puzzle(input_file: &str) -> u64 {
         let calories = file_io::convert_numbers_listfile_to_vec(input_file);
         let mut biggest_sum: u64 = 0;
         let mut current_sum: u64 = 0;
@@ -29,6 +30,33 @@ impl Solution {
 
         biggest_sum
     }
+
+    fn solve_second_puzzle(input_file: &str) -> u64 {
+        let calories = file_io::convert_numbers_listfile_to_vec(input_file);
+        let mut heap = BinaryHeap::new();
+        let mut current_sum: u64 = 0;
+        for calorie in calories {
+            match calorie {
+                None => {
+                    heap.push(current_sum);
+                    current_sum = 0;
+                }
+                Some(num) => {
+                    current_sum += num;
+                }
+            }
+        }
+
+        // Last item
+        heap.push(current_sum);
+
+        let mut sum_of_three_most_calorie_elves = 0;
+        for _ in 0..3 {
+            sum_of_three_most_calorie_elves += heap.pop().unwrap();
+        }
+
+        sum_of_three_most_calorie_elves
+    }
 }
 
 #[cfg(test)]
@@ -36,8 +64,14 @@ mod tests {
     use crate::day_1::puzzle_1::Solution;
 
     #[test]
-    fn print_solution_for_puzzle() {
-        let solution: u64 = Solution::solve_puzzle("inputs/day_1/input.txt");
+    fn print_solution_for_first_puzzle() {
+        let solution: u64 = Solution::solve_first_puzzle("inputs/day_1/input.txt");
+        print!("{}", solution);
+    }
+
+    #[test]
+    fn print_solution_for_second_puzzle() {
+        let solution: u64 = Solution::solve_second_puzzle("inputs/day_1/input.txt");
         print!("{}", solution);
     }
 }
