6fe3df77ac80a40b1f08012c52b8951da290d517
ysngndz <yasinguenduez1@gmail.com>
2022-12-04 09:46:15 +0100

    add a class to find duplicates

--- src/day_3/common_item_finder.rs
+++ src/day_3/common_item_finder.rs
@@ -1,11 +1,30 @@
-pub(crate) struct CommonItemFinder{
-    first: String,
-    second: String,
-    found_iten: Option<String>
+use std::collections::HashSet;
+
+pub(crate) struct CommonItemFinder;
+
+impl CommonItemFinder {
+    pub fn find_item(first: String, second: String) -> String {
+        let set: HashSet<char> = first.chars().collect();
+        for c in second.chars() {
+            if let Some(character) = set.get(&c) {
+                return String::from(*character);
+            }
+        }
+        panic!("There is not such double item!");
+    }
 }
 
-impl CommonItemFinder{
-    pub fn find_item(&self) -> String{
-        todo!()
+#[cfg(test)]
+mod tests {
+    use crate::day_3::common_item_finder::CommonItemFinder;
+
+    #[test]
+    fn returns_correct_duplicate_item() {
+        let first = String::from("afcghtT");
+        let second = String::from("ipmjqTu");
+        assert_eq!(
+            String::from("T"),
+            CommonItemFinder::find_item(first, second)
+        );
     }
 }
