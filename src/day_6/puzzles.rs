b5efb0b1d02143f3dcda7dc80ddba810beb18a8e
ysngndz <yasinguenduez1@gmail.com>
2022-12-13 17:33:51 +0100

    day_6: add boilerplate for day 6

---
+++ src/day_6/puzzles.rs
@@ -0,0 +1,28 @@
+struct Solution;
+
+impl Solution {
+    fn solve_first_puzzle(input_file: &str) -> usize {
+        todo!()
+    }
+
+    fn solve_second_puzzle(input_file: &str) -> usize {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_6::puzzles::Solution;
+
+    #[test]
+    fn solution_for_first_puzzle() {
+        let solution: usize = Solution::solve_first_puzzle("inputs/day_6/input.txt");
+        assert_eq!(0, solution);
+    }
+
+    #[test]
+    fn solution_for_second_puzzle() {
+        let solution: usize = Solution::solve_second_puzzle("inputs/day_6/input.txt");
+        assert_eq!(0, solution);
+    }
+}
