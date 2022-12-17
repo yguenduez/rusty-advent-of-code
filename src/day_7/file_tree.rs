3a72d75e43e505cce6fbd6907451d15045dc715b
ysngndz <yasinguenduez1@gmail.com>
2022-12-17 09:47:24 +0100

    day_7: tree builder

--- src/day_7/file_tree.rs
+++ src/day_7/file_tree.rs
@@ -4,9 +4,9 @@ use std::rc::Rc;
 
 #[derive(PartialEq, Debug, Default)]
 struct Directory {
-    parent_dir: Option<Rc<RefCell<Directory>>>,
     name: String,
     size: usize,
+    parent_dir: Option<Rc<RefCell<Directory>>>,
     child_dirs: Vec<Rc<RefCell<Directory>>>,
     child_files: Vec<Rc<RefCell<File>>>,
 }
@@ -20,8 +20,74 @@ struct File {
 struct TreeBuilder;
 
 impl TreeBuilder {
-    fn from(commands: &[Commands]) -> Directory {
-        todo!()
+    fn from(commands: &[Commands]) -> Rc<RefCell<Directory>> {
+        if commands.len() < 1 {
+            panic!("No commands given!");
+        };
+        let Commands::CdDown(dir_name) = &commands[0] else {
+            panic!(r"First Command must be a 'cd /' command");
+        };
+
+        let root_dir = Directory {
+            name: String::to_string(&dir_name.0),
+            ..Default::default()
+        };
+
+        let root_dir = Rc::new(RefCell::new(root_dir));
+        let mut current_dir = root_dir.clone();
+
+        for command in &commands[1..] {
+            match command {
+                Commands::CdUp => {
+                    let parent_node = current_dir.borrow().parent_dir.clone();
+                    if let Some(node) = parent_node {
+                        current_dir = node;
+                    }
+                }
+                Commands::CdDown(dir_name) => {
+                    let dir = Directory {
+                        name: String::to_string(&dir_name.0),
+                        ..Default::default()
+                    };
+                    let dir_node = Rc::new(RefCell::new(dir));
+                    current_dir.borrow_mut().child_dirs.push(dir_node.clone());
+                    current_dir = dir_node;
+                }
+                Commands::Ls(found_items) => {
+                    found_items
+                        .files
+                        .iter()
+                        .map(|f| File {
+                            name: f.name.0.clone(),
+                            size: f.size,
+                        })
+                        .for_each(|tree_file| {
+                            current_dir
+                                .borrow_mut()
+                                .child_files
+                                .push(Rc::new(RefCell::new(tree_file)))
+                        });
+
+                    found_items
+                        .dirs
+                        .iter()
+                        .map(|dir| Directory {
+                            name: dir.0.clone(),
+                            size: 0,
+                            parent_dir: Some(current_dir.clone()),
+                            child_dirs: vec![],
+                            child_files: vec![],
+                        })
+                        .for_each(|mut dir| {
+                            current_dir
+                                .borrow_mut()
+                                .child_dirs
+                                .push(Rc::new(RefCell::new(dir)));
+                        });
+                }
+            }
+        }
+        root_dir
     }
 }
 
@@ -29,7 +95,7 @@ impl TreeBuilder {
 mod tests {
     use crate::day_7::command_parser::{Commands, DirName, FileName, FileWithSize, LsFinds};
     use crate::day_7::file_tree::{Directory, File, TreeBuilder};
-    use std::cell::RefCell;
+    use std::cell::{Ref, RefCell};
     use std::default;
     use std::rc::Rc;
 
@@ -52,7 +118,7 @@ mod tests {
         };
 
         let dir_y = Directory {
-            name: "x".to_string(),
+            name: "y".to_string(),
             ..Default::default()
         };
 
@@ -74,6 +140,29 @@ mod tests {
             .iter_mut()
             .for_each(|child| child.borrow_mut().parent_dir = Some(rootDir.clone()));
 
-        assert_eq!(rootDir.take(), TreeBuilder::from(&commands));
+        let root_node = TreeBuilder::from(&commands);
+
+        assert_eq!(
+            rootDir.borrow().child_dirs.len(),
+            root_node.borrow().child_dirs.len()
+        );
+
+        for i in 0usize..1usize {
+            let expected_dir = rootDir.borrow().child_dirs[i].clone();
+            let result_dir = root_node.borrow().child_dirs[i].clone();
+            assert_eq!(expected_dir.borrow().name, result_dir.borrow().name);
+
+            let expected_root_node = rootDir.clone();
+            let result_root_node = result_dir.borrow().parent_dir.clone().unwrap();
+
+            assert_eq!(
+                expected_root_node.borrow().name,
+                result_root_node.borrow().name
+            );
+        }
+
+        let expected_file = rootDir.borrow().child_files[0].clone();
+        let result_file = root_node.borrow().child_files[0].clone();
+        assert_eq!(expected_file.borrow().name, result_file.borrow().name);
     }
 }
